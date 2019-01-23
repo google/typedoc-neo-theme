5c0c0d3a56e4929397c458df0a252f218efe6b7d
Nick Felker <fleker@fleker-macbookpro.roam.corp.google.com>
2019-01-23 16:40:15 -0500

    Updates build and packaging instructions
    Tested with actions-on-google library to
    verify correct output
    
    Change-Id: I7655f7f6653b004cbe02f2678f541f8b5a6a954d

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-let plugin = require('./plugin')
+let plugin = require('./third_party/src/default/plugin')
 module.exports = (PluginHost) => {
   const app = PluginHost.owner
   /*
