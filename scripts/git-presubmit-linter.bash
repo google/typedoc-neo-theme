fc129d3e75d58a4e196c4cc6bbafa4d7f585a487
Nick Felker <fleker@google.com>
2019-12-20 17:56:52 -0500

    Embed script with IFS-space to see if it passes
    Change-Id: Iaf5a497147efe46574552c57dc37eb978044b183

--- scripts/git-presubmit-linter.bash
+++ scripts/git-presubmit-linter.bash
@@ -31,5 +31,28 @@ fi
 set +e
 IFS=$' ' # Separate input by space
 # Run "Verify package contents"
+EXPECT=$(cat ./scripts/expected-files.txt)
 TAR=$(yarn pack | grep -Eo "/.*tgz")
-tar -tf "${TAR}" | ./git-presubmit-linter/tools/filelist.sh ./scripts/expected-files.txt
+TARF=$(tar -tf ${TAR})
+REJECTED_FILES=0
+while read tarFile
+do
+    VALID=0
+    while read pattern
+    do
+        echo "$tarFile" | grep -Po "^$pattern\$"
+        if [ $? -eq 0 ]; then
+            # Package file matches a valid pattern
+            VALID=1
+            break
+        fi
+    done <<< $EXPECT
+    if [ $VALID -ne 1 ]; then
+        echo "$tarFile does not match any pattern"
+        REJECTED_FILES=$((REJECTED_FILES + 1))
+    fi
+done <<< $TARF
+if [ $REJECTED_FILES -gt 0 ]; then
+    echo "Filelist check failed ($REJECTED_FILES item(s) invalid)"
+    exit 1
+fi
