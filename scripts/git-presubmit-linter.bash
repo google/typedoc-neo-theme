51d06034e0abef40e43fdf74f16e7a3a1d8f3948
Nick Felker <fleker@google.com>
2020-02-11 12:02:03 -0500

    Update script with new IFS param
    Change-Id: I5dc978b438b4aa234705041379741619380f5f8a

--- scripts/git-presubmit-linter.bash
+++ scripts/git-presubmit-linter.bash
@@ -28,31 +28,6 @@ else
     cd ../
 fi
 
-set +e
-IFS=$' ' # Separate input by space
 # Run "Verify package contents"
-EXPECT=$(cat ./scripts/expected-files.txt)
 TAR=$(yarn pack | grep -Eo "/.*tgz")
-TARF=$(tar -tf ${TAR})
-REJECTED_FILES=0
-while read tarFile
-do
-    VALID=0
-    while read pattern
-    do
-        echo "$tarFile" | grep -Po "^$pattern\$" > /dev/null
-        if [ $? -eq 0 ]; then
-            # Package file matches a valid pattern
-            VALID=1
-            break
-        fi
-    done <<< $EXPECT
-    if [ $VALID -ne 1 ]; then
-        echo "$tarFile does not match any pattern"
-        REJECTED_FILES=$((REJECTED_FILES + 1))
-    fi
-done <<< $TARF
-if [ $REJECTED_FILES -gt 0 ]; then
-    echo "Filelist check failed ($REJECTED_FILES item(s) invalid)"
-    exit 1
-fi
+tar -tf "${TAR}" | ./git-presubmit-linter/tools/filelist.sh ./scripts/expected-files.txt " "
