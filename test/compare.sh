2e970c7d214bfa954e88542e631f7388efec4c4d
akshanshck <123233309+akshanshck@users.noreply.github.com>
2023-02-04 22:54:42 +0530

    Update compare.sh

--- test/compare.sh
+++ test/compare.sh
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+echo "[!] Comparing..."
 set -e
 
 # Get every 'expected/' file and compare it against the newly-generated files
